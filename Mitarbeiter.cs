7ae530dd1936845566d15dbf1413517034b39c20
Bhajan Gorgs <baiangorgs@gmail.com>
2025-11-26 15:50:18 +0100

    Add properties and methods to Mitarbeiter class

--- Mitarbeiter.cs
+++ Mitarbeiter.cs
@@ -1,4 +1,19 @@
 public class Mitarbeiter
 {
+     public string Name { get; set; }
+     public string Nachname { get; private set;} 
+     public int Urlaubstage { get; private set; }
+     public List<DateTime> Krankheitstage { get; private set; } 
 
+  public Mitarbeiter( string Name, string Nachname, int Urlaubstage )
+
+    this.Name = Name;
+    this.Nachname = Nachname;
+    this.Urlaubstage = Urlaubstage;
+    this.Krankheitstage = new List<DateTime>();
+
+  public void AddKrankheitstag(DateTime Krankheitstag)
+  {
+    Krankheitstage.Add(Krankheitstag);
+  }
 }
